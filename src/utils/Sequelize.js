0df3356779af36ac6c75652be18e92652dfd50af
Marcos Knudsen <marcosknu14@gmail.com>
2024-12-03 18:45:08 -0300

    parametrizar conexion

--- src/utils/Sequelize.js
+++ src/utils/Sequelize.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("PROMIEDOS_BASQUET", "root", "kanu", {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  port: process.env.DB_PORT,
-});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
 
 module.exports = sequelize;
