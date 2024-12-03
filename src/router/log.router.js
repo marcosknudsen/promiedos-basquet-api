304912cadf9fd294b44761d1c43920a0e466b0e0
Marcos Knudsen <marcosknu14@gmail.com>
2024-12-03 19:29:18 -0300

    catch errors

--- src/router/log.router.js
+++ src/router/log.router.js
@@ -8,15 +8,22 @@ router.get("/logs", (req, res) => {
 
 router.post("/logs", async (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const { Action, League, Team } = req.body;
   await Logs.sync();
 
-  const log = await Logs.create({
-    Ip: ip,
-    Date: new Date(),
-    Action: "GET",
-  });
-
-  res.send(log);
+  try{ 
+    const log = await Logs.create({
+      Ip: ip,
+      Date: new Date(),
+      Action: Action,
+      League_Id: League,
+      Team_Id: Team,
+    });
+    res.send(log);
+  }
+  catch(err){
+    res.send(err);
+  }
 });
 
 module.exports = router;
